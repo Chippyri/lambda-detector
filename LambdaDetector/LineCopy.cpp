b361641ec747f9c2f0526feaf1b52b399424bab7
heho1602 <29439581+Chippyri@users.noreply.github.com>
2020-04-10 22:12:53 +0200

    Created example for parsing the test.txt file and getting the preceding and following lines from a file

---
+++ LambdaDetector/LineCopy.cpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <fstream>
+#include <string>
+#include <iostream>
+using std::ifstream;
+using std::string;
+using std::cout;
+using std::endl;
+using std::getline;
+using std::stoi;
+
+int main(const int argc, const char* argv[])
+{
+	ifstream testFile("test.txt");
+	string firstLine;
+	string delimiter = " line: ";
+	getline(testFile, firstLine);
+	testFile.close();
+	auto position = firstLine.find(delimiter);
+	auto endPosition = position + 7;
+	string token = firstLine.substr(0, position);
+	string endToken = firstLine.substr(endPosition);
+	cout << firstLine << endl;
+	cout << token << endl;
+	cout << endToken << endl;
+	auto value = stoi(endToken);
+	cout << value << endl;
+
+	ifstream firstFile(token);
+	auto linecounter = 0;
+	string tempStr;
+
+	int numberOfLines = 8;
+	int startLine = value - (numberOfLines / 2); // Center on line
+
+	if (startLine >= -1)
+	{
+		while (getline(firstFile, tempStr)){
+			linecounter++;
+			if (linecounter == startLine)
+			{
+				cout << tempStr << endl;
+				for (auto i = 0; i < numberOfLines; i++)
+				{
+					getline(firstFile, tempStr);
+					cout << tempStr << endl;
+				}
+			}
+		}
+	}
+	
+	return 0;
+}
