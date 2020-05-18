eb9985721cd771d20c06686e0f6ac2ba243d7728
heho1602 <29439581+Chippyri@users.noreply.github.com>
2020-05-18 22:37:18 +0200

    Updated comment counter to count curly braces

--- LambdaDetector/CommentCounter.cpp
+++ LambdaDetector/CommentCounter.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <boost/algorithm/string/replace.hpp>
 #include <regex>
+#include <algorithm>
 
 using std::ifstream;
 using std::ofstream;
@@ -17,6 +18,7 @@ using std::stoi;
 using std::vector;
 using std::regex;
 using std::regex_search;
+using std::count;
 
 bool searchAndWriteToFileIfExisted(const string& match, const regex& reg, ofstream& file, const string& path)
 {
@@ -35,23 +37,22 @@ bool containsAComment(const string & tmpStr)
 	return tmpStr.find("//") != string::npos || tmpStr.find("/*") != string::npos;
 }
 
-bool containsACurlyBrace(const string& tmpStr)
+bool containsStartingCurlyBrace(const string& tmpStr)
+{
+	return tmpStr.find('{') != string::npos;
+}
+
+bool containsEndingCurlyBrace(const string& tmpStr)
 {
 	return tmpStr.find('}') != string::npos;
 }
 
-
-
 int main(const int argc, const char* argv[])
-{
-	// Regexes
-	
+{	
 	// Input file
 	const string INPUT_FILE_NAME = "test.txt";
 	const string DELIMITER = " line: ";
 	const auto DELIMITER_LENGTH = DELIMITER.length();
-
-
 
 	// Open the file with the LambdaDetector-results.
 	ifstream inputFile(INPUT_FILE_NAME);
@@ -61,29 +62,34 @@ int main(const int argc, const char* argv[])
 	}
 	
 	// Files to output to
-	ofstream beforeLambdaFile("comment_before_lambda.txt", ofstream::out | ofstream::trunc);
-	ofstream onSameLineFile("comment_on_same_line.txt", ofstream::out | ofstream::trunc);
-	ofstream inLambdaFile("comment_in_lambda.txt", ofstream::out | ofstream::trunc);
-	ofstream rightAfterLambdaFile("comment_right_after_lambda.txt", ofstream::out | ofstream::trunc);
+	ofstream aboveLambdaFile("comment_above_lambda.txt", ofstream::out | ofstream::trunc);
+	ofstream insideLambdaFile("comment_inside_lambda.txt", ofstream::out | ofstream::trunc);
 
-	// Counters
-	int commentOnLineBeforeLambda = 0;
-	int commentOnSameRowAsLambda = 0;
-	int commentInsideTheLambda = 0;
-	int commentRightAfterLambda = 0;
+	ofstream oneLinersFile("comment_one_liners.txt", ofstream::out | ofstream::trunc);
+	ofstream testFile("comment_test.txt", ofstream::out | ofstream::trunc);
+	ofstream faultFile("comment_faults.txt", ofstream::out | ofstream::trunc);
 	
+	// Counters
+	int commentAboveLambda = 0;
+	int commentInsideLambda = 0;
+	int noCommentsAtAll = 0;
+	int hadSomeComment = 0;
+	int faults = 0;
+	int lambdasChecked = 0;
+	int oneLiners = 0;
+
 	// Read from input file and copy the corresponding rows to a string to analyse
 	string currentInputLine;
 	while (getline(inputFile, currentInputLine))
 	{
+		lambdasChecked++;
+		
 		// Read the values from file
 		const auto delimiterPosition = currentInputLine.find(DELIMITER);
 		const auto pastDelimiterPosition = delimiterPosition + DELIMITER_LENGTH;
 		const string filePath = currentInputLine.substr(0, delimiterPosition);
 		const string rowNumberAsString = currentInputLine.substr(pastDelimiterPosition);
 		const auto rowNumberAsInt = stoi(rowNumberAsString);
-
-		cout << currentInputLine << endl;
 
 		// Open file and read specified rows
 		ifstream fileToCopyFrom(filePath);
@@ -98,9 +104,13 @@ int main(const int argc, const char* argv[])
 		auto inFileLineCounter = 0;
 
 		bool lineBeforeChecked = false;
-		bool lineWithLambdaChecked = false;
-		bool linesInsideLambdaChecked = false;
-		bool haveFoundACommentInsideLambda = false;
+		bool lineWithLambdaStartChecked = false;
+		bool lambdaHadACommentInside = false;
+		bool lambdaHadACommentAbove = false;
+		
+		bool foundStartingBrace = false;
+		bool foundEndingBrace = false;
+		auto bracesCounter = 0;
 						
 		while (getline(fileToCopyFrom, tmpStr)) {
 			inFileLineCounter++;
@@ -111,53 +121,101 @@ int main(const int argc, const char* argv[])
 				{
 					if (containsAComment(tmpStr))
 					{
-						beforeLambdaFile << tmpStr << endl;
-						commentOnLineBeforeLambda++;
+						aboveLambdaFile << currentInputLine << endl;
+						aboveLambdaFile << tmpStr << endl;
+						lambdaHadACommentAbove = true;
+						commentAboveLambda++;
 					}
 					lineBeforeChecked = true;
 					
-				} else if (!lineWithLambdaChecked)
+				} else if (!lineWithLambdaStartChecked)
 				{
-					// Check the row with the lambda, does it have a comment?
-					if(containsAComment(tmpStr))
-					{
-						commentOnSameRowAsLambda++;
-						onSameLineFile << tmpStr << endl;
-					}
-					lineWithLambdaChecked = true;
+					// Count the braces on the row
+					auto startingBraces = count(tmpStr.begin(), tmpStr.end(), '{');
+					auto endingBraces = count(tmpStr.begin(), tmpStr.end(), '}');
+					bracesCounter = startingBraces - endingBraces;
+
+					//cout << bracesCounter;
 					
-				}
-				else if (!linesInsideLambdaChecked)
-				{
-					// Does the lambda end?
-					if (containsACurlyBrace(tmpStr))
+					// Likely one-liners
+					if (bracesCounter == 0 && startingBraces > 0)
 					{
-						// Check right after the lambda ending brace, does it have a comment?
-						linesInsideLambdaChecked = true;
-						if(containsAComment(tmpStr))
-						{
-							commentRightAfterLambda++;
-							rightAfterLambdaFile << tmpStr << endl;
-						}
-					} else
+						oneLinersFile << tmpStr << endl;
+						oneLiners++;
+					}
+					// No braces at all
+					else if (bracesCounter == 0) {
+						// TODO: Find the starting brace
+						faultFile << tmpStr << endl;
+						faults++;
+					}
+					// More start braces than end braces, ideal
+					else if (bracesCounter == 1)
 					{
-						// Check inside the lambda, does it have a comment?
-						if (!haveFoundACommentInsideLambda && containsAComment(tmpStr))
+						foundStartingBrace = true;
+					}
+					// Multiple start braces
+					else if (bracesCounter > 1)
+					{
+						// TODO: Possibly just subtract by one?
+						faultFile << tmpStr << endl;
+						faults++;
+					}
+					// More end braces than start braces
+					else
+					{
+						// TODO: Substract the end braces?
+						faultFile << tmpStr << endl;
+						faults++;
+					}
+					lineWithLambdaStartChecked = true;
+				} else if(foundStartingBrace && !foundEndingBrace && bracesCounter != 0){
+
+					// NOTE: Just a single comment inside each lambda
+					if (!lambdaHadACommentInside && containsAComment(tmpStr))
+					{
+						lambdaHadACommentInside = true;
+						insideLambdaFile << currentInputLine << endl;
+						insideLambdaFile << tmpStr << endl;
+						commentInsideLambda++;
+					}
+					
+					bracesCounter += count(tmpStr.begin(), tmpStr.end(), '{');
+					bracesCounter -= count(tmpStr.begin(), tmpStr.end(), '}');
+
+					if (bracesCounter == 0)
+					{
+						foundEndingBrace = true;
+						if (!lambdaHadACommentAbove && !lambdaHadACommentInside)
 						{
-							haveFoundACommentInsideLambda = true;
-							inLambdaFile << tmpStr << endl;
-							commentInsideTheLambda++;
+							noCommentsAtAll++;
+						} else if (lambdaHadACommentAbove || lambdaHadACommentInside)
+						{
+							hadSomeComment++;
 						}
+						
+						cout << "f";
 					}
 				}
 				else { break; }
 			}
 		}
 	}
-	cout << "Before: " << commentOnLineBeforeLambda << endl;
-	cout << "On the same line: " << commentOnSameRowAsLambda << endl;
-	cout << "Inside lambda: " << commentInsideTheLambda << endl;
-	cout << "Right after end of lambda: " << commentRightAfterLambda << endl;
+
+	aboveLambdaFile << "Count: " << commentAboveLambda << endl;
+	insideLambdaFile << "Count: " << commentInsideLambda << endl;
+	faultFile << "Count: " << faults << endl;
+
+	cout << endl << "COMMENTS" << endl;
+	cout << "Right above lambda: " << commentAboveLambda << endl;
+	cout << "Inside the lambda: " << commentInsideLambda << endl;
+
+	cout << "AMOUNTS" << endl;
+	cout << "Lambdas checked: " << lambdasChecked << endl;
+	cout << "One-liners: " << oneLiners << endl;
+	cout << "Faults: " << faults << endl;
+	cout << "No comments at all: " << noCommentsAtAll << endl;
+	cout << "Had some comment: " << hadSomeComment << endl;
 	
 	return 0;
 }
